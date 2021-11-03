8f92c445c3b6362794d46311cb6a12eef9057cbc
Peter Abah <peterabah.ob@gmail.com>
2021-11-03 16:56:48 +0100

    edit collectionDomManager.js

---
+++ src/collectionDomManager.js
@@ -0,0 +1,53 @@
+import PubSub from 'pubsub-js';
+import eventTypes from './eventTypes.js';
+
+const collectionDomManager = (() => {
+  const addEventListeners = () => {
+    collectionBtns.forEach(btn => 
+      btn.addEventListener('click', showCollection)
+    );
+
+    addCollectionBtn.addEventListener('click', showCollectionForm);
+
+    collectionForm.addEventListener('click', createCollection);
+  };
+
+  const showCollection = event => {
+    const collectionName = event.target.getAttribute('data-collection');
+    PubSub.publish(eventTypes.SHOW_TODOS, {collectionName});
+  };
+
+  const showCollectionForm = event => {
+    collectionForm.classList.toggle('collection__form--hidden');
+  };
+
+  const createCollection = event => {
+    event.preventDefault();
+    const collectionName = collectionInput.value;
+
+    PubSub.publish(eventTypes.NEW_COLLECTION, {name: collectionName});
+  };
+
+  const addCollection = ({name}) => {
+    const btn = document.createElement('button');
+    btn.classList.add('collection__btn');
+
+    btn.textContent = name;
+    btn.setAttribute('data-collection', name);
+
+    btn.addEventListener('click', showCollection);
+    collectionBtnsContainer.appendChild(btn);
+  };
+
+  const collectionBtnsContainer = document.querySelector('.collection');
+  const collectionBtns = [...document.querySelectorAll('.collection__btn')];
+  const addCollectionBtn = document.querySelector('.collection__btn-add');
+  const collectionForm = document.querySelector('.collection__form');
+  const collectionInput = document.getElementById('collection-name');
+
+  PubSub.subscribe(eventTypes.NEW_COLLECTION, addCollection);
+
+  addEventListeners();
+})();
+
+export default collectionDomManager;
