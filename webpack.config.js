2af5c1525f9189eb9bc9faa012ea5d5d1fe965f0
Peter Abah <peterabah.ob@gmail.com>
2021-11-03 13:05:06 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Todo',
+    }),
+  ]
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(handlebars|hbs)$/i,
+        loader: 'handlebars-loader',
+      },
+    ],
+  },
+}
