01290cff24f5e3ff9e34ee78db683f63fa9c4ca3
Peter Abah <peterabah.ob@gmail.com>
2021-11-09 09:17:51 +0100

    add hiding sidebar in mobike device

--- src/ui/category.js
+++ src/ui/category.js
@@ -6,20 +6,23 @@ const category = (() => {
     categoryBtns.forEach(btn => 
       btn.addEventListener('click', showCategory)
     );
+
+    menuBtn.addEventListener('click', toggleSidebr)
   };
 
   const showCategory = event => {
     const category = event.target.getAttribute('data-category');
-    
+    toggleSidebr();
     PubSub.publish(eventTypes.SHOW_TODOS, {category});
   };
 
   const toggleSidebr = event => {
-    
+    sidebar.classList.toggle('sidebar--hidden')
   }
 
   const categoryBtns = [...document.querySelectorAll('.category')];
-  const sidebar = document.querySelector('.sidebar')
+  const sidebar = document.querySelector('.sidebar');
+  const menuBtn = document.querySelector('.menu-btn')
   addEventListeners();
 })();
 
